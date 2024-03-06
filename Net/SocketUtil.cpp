69a7488e059c52492c2f48039c33b5c1e119d79e
SUNG CHAN CHOI <73463402+brown0394@users.noreply.github.com>
2024-03-06 17:13:41 +0900

    add select function

--- Net/SocketUtil.cpp
+++ Net/SocketUtil.cpp
@@ -29,3 +29,44 @@ int SocketUtil::GetLastError() {
 void SocketUtil::ReportError(const wchar_t* err) {
 	printf("%ls\n", err);
 }
+
+fd_set* SocketUtil::FillSetFromVector(fd_set& outSet, const std::vector<TCPSocketPtr>* inSockets) {
+	if (inSockets) {
+		FD_ZERO(&outSet);
+		for (const TCPSocketPtr& socket : *inSockets) {
+			FD_SET(socket->mSocket, &outSet);
+		}
+		return &outSet;
+	}
+	else {
+		return nullptr;
+	}
+}
+
+void SocketUtil::FillVectorFromSet(std::vector<TCPSocketPtr>* outSockets,
+const std::vector<TCPSocketPtr>* inSockets,	const fd_set& inSet) {
+	if (inSockets && outSockets) {
+		outSockets->clear();
+		for (const TCPSocketPtr& socket : *inSockets) {
+			if (FD_ISSET(socket->mSocket, &inSet)) {
+				outSockets->push_back(socket);
+			}
+		}
+	}
+}
+
+int SocketUtil::Select(const std::vector<TCPSocketPtr>* inReadSet, std::vector<TCPSocketPtr>* outReadSet,
+	const std::vector<TCPSocketPtr>* inWriteSet, std::vector<TCPSocketPtr>* outWriteSet,
+	const std::vector<TCPSocketPtr>* inExceptSet, std::vector<TCPSocketPtr>* outExceptSet) {
+	fd_set read, write, except;
+	fd_set* readPtr = FillSetFromVector(read, inReadSet);
+	fd_set* writePtr = FillSetFromVector(read, inWriteSet);
+	fd_set* exceptPtr = FillSetFromVector(read, inExceptSet);
+	int toRet = select(0, readPtr, writePtr, exceptPtr, nullptr);
+	if (toRet > 0) {
+		FillVectorFromSet(outReadSet, inReadSet, read);
+		FillVectorFromSet(outWriteSet, inWriteSet, write);
+		FillVectorFromSet(outExceptSet, inExceptSet, except);
+	}
+	return toRet;
+}
--- Net/SocketUtil.h
+++ Net/SocketUtil.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "UDPSocket.h"
 #include "TCPSocket.h"
+#include <vector>
+
 enum SocketAddressFamily {
 	INET = AF_INET,
 	INET6 = AF_INET6
@@ -11,5 +13,11 @@ public:
 	TCPSocketPtr CreateTCPSocket(SocketAddressFamily inFamily);
 	static int GetLastError();
 	static void ReportError(const wchar_t* err);
+	fd_set* FillSetFromVector(fd_set& outSet, const std::vector<TCPSocketPtr>* inSockets);
+	void FillVectorFromSet(std::vector<TCPSocketPtr>* outSockets, const std::vector<TCPSocketPtr>*
+		inSockets, const fd_set& inSet);
+	int Select(const std::vector<TCPSocketPtr>* inReadSet, std::vector<TCPSocketPtr>* outReadSet,
+		const std::vector<TCPSocketPtr>* inWriteSet, std::vector<TCPSocketPtr>* outWriteSet,
+		const std::vector<TCPSocketPtr>* inExceptSet, std::vector<TCPSocketPtr>* outExceptSet);
 };
 
